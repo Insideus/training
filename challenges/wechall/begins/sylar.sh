413d1cd7c63a0616befc97e050508b4f53791829
SYLAR <cristosantos@gmail.com>
2017-07-11 17:37:22 -0500

    Create sylar.sh

---
+++ challenges/wechall/begins/sylar.sh
@@ -0,0 +1,6 @@
+cat /home/level//README.txt | grep bitwarrior
+cat /home/level/1/blue/pill/hats/gray/solution/is/SOLUTION.txt | grep LameStartup
+cat /home/level/2/porb/.solution | grep HiddenIsConfig
+cat /home/level/3/.bash_history
+chown nickname /home/user/nickname/level/4/README.txt
+cat /home/user/nickname/level/4/README.txt | grep AndIknowchown
