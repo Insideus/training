db8de26ca05cab3b21e6c10c47171edb606346d2
SYLAR <cristosantos@gmail.com>
2017-07-13 19:32:54 -0500

    Create SYLAR.sh

---
+++ challenges/wechall/can-you-read-me/SYLAR.sh
@@ -0,0 +1,9 @@
+wget -O ocr.png --no-cookies --header "Cookie:WC=" \
+	http://www.wechall.net/challenge/can_you_readme/gimme.php
+
+tesseract ocr.png ocr -psm 7
+
+SOLUTION=head -1 ocr.txt
+
+curl -b "WC=" -G -d "solution=$SOLUTION" -d "cmd=Answer" \
+	http://www.wechall.net/challenge/can_you_readme/index.php
