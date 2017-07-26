cf998575a5734aba5093667e755ce9714cef2d7a
SYLAR <cristosantos@gmail.com>
2017-07-26 16:09:36 -0500

    Create Sylar.sh

---
+++ challenges/codeabbey/052/Sylar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -r n1
+for i in $(seq -s ' ' 1 $n1)
+do
+    read -r n2 n3 n4
+    l1=$(python -c "print($n2 ** 2)")
+    l2=$(python -c "print($n3 ** 2)")
+    l3=$(python -c "print($n4 ** 2)")
+    if [ "$(($l1 + $l2))" -eq $l3 ]; then
+        respuesta="${respuesta} R"
+    elif [ "$(($l1 + $l2))" -lt $l3 ]; then
+        respuesta="${respuesta} O"
+    else
+        respuesta="${respuesta} A"
+    fi
+done
+
+echo "$respuesta"
