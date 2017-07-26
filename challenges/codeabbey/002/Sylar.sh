8ba2a855ba5c929e5f5cc2a707e94b5b432686c9
SYLAR <cristosantos@gmail.com>
2017-07-25 20:07:39 -0500

    Create Sylar.sh

---
+++ challenges/codeabbey/002/Sylar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -r n1
+read -p "$n1" -r name
+suma=0
+#cont=0
+for i in $name; do
+    suma=$((suma + i))
+    #cont=$(($cont + 1))
+    #echo "acaaaaaaa $cont ---------$suma"
+done
+echo "Es $suma!"
