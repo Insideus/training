0663e6f52ba16bc0b007903602ec746971feab36
SYLAR <cristosantos@gmail.com>
2017-07-27 18:51:04 -0500

    Create Sylar.c

---
+++ challenges/codeabbey/047/Sylar.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+
+#define decrypt_rot(x, y) rot((26-x), y)
+ 
+void rot(int c, char *str)
+{
+	int l = strlen(str);
+	const char *alpha[2] = { "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+ 
+	int i;
+	for (i = 0; i < l; i++)
+	{
+		if (!isalpha(str[i]))
+			continue;
+ 
+		str[i] = alpha[isupper(str[i])][((int)(tolower(str[i])-'a')+c)%26];
+	}
+}
+ 
+ 
+int main()
+{
+	FILE *archivo;
+	int lineas, key,i=0;
+	char a[100], *ch;
+	archivo = fopen("cesar.txt","r");
+	if (archivo == NULL)
+   {
+   		 printf("\nError de apertura del archivo. \n\n");
+   }
+   else
+    {
+    		
+            fgets(a,100,archivo);
+            ch = strtok(a, " ");
+            lineas=atoi(ch);
+ 			printf("%d\n", lineas);
+  			ch = strtok(NULL, " ");
+  			key=atoi(ch);
+  			printf("%d\n", key);
+  			while (feof(archivo) == 0 && lineas>0)
+ 			{
+ 				fgets(a,100,archivo);
+ 				decrypt_rot(key,a);
+ 				printf("%s ",a);
+ 				lineas--;
+ 				i++;
+ 			}
+
+            //lineas=atoi(a);
+            //printf("%d",lineas);
+           
+	}
+ 	fclose(archivo);
+	return 0;
+}
